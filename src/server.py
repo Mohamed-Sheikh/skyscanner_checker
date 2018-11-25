ddd8d3c7aee1fc9c3394e26ef8ba806d401dd35d
Senna Semakula-Buuz <sennasemakul@DESKTOP-26SE7DD.localdomain>
2018-11-25 15:48:14 +0000

    Template routing

---
+++ src/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import os
+
+"""Main application file"""
+app = Flask(__name__)
+curr_dir = os.getcwd()
+
+@app.route('/')
+def index():
+    file_name = '{}/templates/index.html'.format(curr_dir)
+    print(file_name)
+    print("@@@@@@")
+    render_template(file_name)
+
+"""When routed, will display results of ticket prices"""
+@app.route('/results')
+def results():
+    pass
+
+"""Main entry point for application"""
+def main():
+    app.run(debug=True)
+
+main()
+
