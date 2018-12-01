48f6a39aa2b3276eb08c6c96b3f22b4090511300
Senna Semakula-Buuza <ssemakula@cisco.com>
2018-12-01 15:53:44 +0000

    Added functionst to insert user data

--- app/bot.py
+++ app/bot.py
@@ -4,19 +4,34 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 
 import os
+import logging
+import user_input
 
 options = Options()
 options = webdriver.ChromeOptions()
 
 options.add_argument("--disable-dev-shm-usage"); # overcome limited resource problems
 options.add_argument("--no-sandbox"); # Bypass OS security model
-driver_path = "{}/chromedriver".format(os.getcwd())
+driver_path = "{}/app/lib/chromedriver".format(os.getcwd())
 URL = "https://www.skyscanner.net"
-#URL = "http:127.0.0.1:5000/"
-print("path is : {}".format(driver_path))
+URL = "http:127.0.0.1:5000/"
 driver = webdriver.Chrome(driver_path, options=options)
 driver.get(URL)
 
-dest_search = driver.find_element_by_name('from_dest')
-driver.send_keys('loooooool')
+flight_data = user_input.flight_data
 
+def insert_by_name(name, val):
+    """Identifies html components and sends value"""
+    try:
+        name = driver.find_element_by_name(name)
+    except Exception as e:
+        raise(e)
+    else:
+        name.send_keys(val)
+
+def search_flights(data):
+    for k, v in data.items():
+        insert_by_name(k, v)
+
+if __name__ == '__main__':
+    search_flights(flight_data)
